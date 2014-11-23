a7486dbd747b422530554c136c624a1cd679d5a4
miraks <a@vldkn.net>
2014-11-24 00:57:38 +0300

    fix smart serializer work for objects

--- app/services/serializer/smart.rb
+++ app/services/serializer/smart.rb
@@ -1,8 +1,17 @@
 module Serializer
   class Smart < Base
     def serialize **opts
-      serializer = item.class.name.demodulize.prepend('Serializer::').constantize
-      serializer.new(data).serialize(opts)
+      find_serializer.new(data).serialize(opts)
+    end
+
+    private
+
+    def find_serializer
+      case item
+      when ::Array then Serializer::Array
+      when ActiveRecord::Relation then Serializer::Relation
+      else Serializer::Object
+      end
     end
   end
 end
