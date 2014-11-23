f8711828c586b9e8c5ad0c47762de039eb52bba1
miraks <a@vldkn.net>
2014-11-23 20:51:07 +0300

    fix react initialization

--- app/helpers/react_helper.rb
+++ app/helpers/react_helper.rb
@@ -1,7 +1,7 @@
 module ReactHelper
   def react_render path, props: nil, tag: :div, prerender: true, **options
     options[:data] ||= {}
-    options[:data][:path] = path
+    options[:data][:component] = path
     options[:data][:props] = props.to_json
     content = react_context.render(path, props).html_safe if prerender
     content_tag tag, content, options
