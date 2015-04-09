462a0695a605bae6880b7f2a2f3b8a601b815733
Anton Kalinin <asgard.ssh@gmail.com>
2015-04-09 17:48:01 +0300

    add user to ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,5 @@
 class Rating < ActiveRecord::Base
+  belongs_to :user
   belongs_to :section
   has_many :ratings_tags, dependent: :destroy
   has_many :tags, through: :ratings_tags
--- app/serializers/rating_serializer.rb
+++ app/serializers/rating_serializer.rb
@@ -1,6 +1,7 @@
 class RatingSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :created_at
 
+  has_one :user
   has_one :section
   has_many :tags
 end
