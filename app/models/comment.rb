4561de6901c1f1326e6f4ccbbf0e6c805c336071
miraks <a@vldkn.net>
2015-04-24 21:32:24 +0300

    fix comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :rating
   belongs_to :parent, class_name: 'Comment'
-  has_many :children, foreign_key: :parent_id, class_name: 'Comment'
+  has_many :children, foreign_key: :parent_id, class_name: 'Comment', dependent: :destroy
 
-  validates :body, :user, :rating, null: false
+  validates :body, :user, :rating, presence: true
 end
