bbabd482966eec813e23a0792d02b5a2111ac572
miraks <a@vldkn.net>
2015-05-21 21:01:48 +0300

    correctly update likes count after like destroy

--- app/controllers/api/private/ratings/likes_controller.rb
+++ app/controllers/api/private/ratings/likes_controller.rb
@@ -6,12 +6,13 @@ module Api
 
         def create
           like = ::Likes::Create.new(@rating, current_user).call
-          render json: like, meta: { likes_count: like.rating.likes_count }
+          render json: like, meta: { likes_count: @rating.likes_count }
         end
 
         def destroy
           like = ::Likes::Destroy.new(@rating, current_user).call
-          render json: { success: like.destroyed?, meta: { likes_count: like.rating.likes_count } }
+          @rating.reload
+          render json: { success: like.destroyed?, meta: { likes_count: @rating.likes_count } }
         end
       end
     end
