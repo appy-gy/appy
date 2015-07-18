ffadd238fcddf98982830dd39067894b58aa2fb9
miraks <a@vldkn.net>
2015-07-18 19:33:23 +0300

    remove backward compatibility code in migrations

--- db/migrate/20150517143042_add_slug_to_users.rb
+++ db/migrate/20150517143042_add_slug_to_users.rb
@@ -1,15 +1,6 @@
 class AddSlugToUsers < ActiveRecord::Migration
-  def up
-    add_column :users, :slug, :text
-    User.find_each do |user|
-      user.send :set_slug
-      user.save
-    end
-    change_column_null :users, :slug, false
+  def change
+    add_column :users, :slug, :text, null: false
     add_index :users, :slug, unique: true
-  end
-
-  def down
-    remove_column :users, :slug
   end
 end
