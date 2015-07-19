7e96be3b1396380d9fffe08809f2f40582db6595
miraks <a@vldkn.net>
2015-07-19 22:41:09 +0300

    return assets related configurtion in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,16 @@ Rails.application.configure do
   # Disable Rails's static asset server (Apache or NGINX will already do this).
   config.serve_static_files = false
 
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
