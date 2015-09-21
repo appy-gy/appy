991795a50ea01a6a6a1cc1e966286543fe57e11d
miraks <a@vldkn.net>
2015-09-21 17:05:19 +0300

    disable newrelic in development

--- prerender/newrelic.js
+++ prerender/newrelic.js
@@ -3,13 +3,11 @@ var path = require('path');
 var config = {
   app_name: ['Appy'],
   license_key: process.env.TOP_NEW_RELIC_LICENSE_KEY,
+  agent_enabled: process.env.TOP_ENV == 'production',
   logging: {
     level: 'info',
     filepath: path.join(__dirname, '../log/newrelic_prerender.log')
   },
-  browser_monitoring: {
-    enable: process.env.TOP_ENV == 'production'
-  }
 };
 
 module.exports.config = config;
