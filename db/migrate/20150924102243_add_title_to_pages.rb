22648198c15d2634b6fe23a1cb7a6e57738e5821
miraks <a@vldkn.net>
2015-09-24 13:41:00 +0300

    add temp title to existing pages

--- db/migrate/20150924102243_add_title_to_pages.rb
+++ db/migrate/20150924102243_add_title_to_pages.rb
@@ -1,5 +1,11 @@
 class AddTitleToPages < ActiveRecord::Migration
-  def change
-    add_column :pages, :title, :text, null: false
+  def up
+    add_column :pages, :title, :text
+    Page.find_each { |page| page.update title: 'temp' }
+    change_column_null :pages, :title, false
+  end
+
+  def down
+    remove_column :pages, :title
   end
 end
