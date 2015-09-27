838554fa833c895d92223fda51e8a30aef63ce43
Anton Kalinin <asgard.ssh@gmail.com>
2015-09-27 18:55:01 +0300

    update oauths controller

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -7,20 +7,30 @@ class OauthsController < ApplicationController
 
   def callback
     provider = auth_params[:provider]
-
     return if login_from provider
-
-    user = create_and_validate_from provider
-    user.generate_password
-    user.generate_email
-    user = User.find_by(email: user.email) unless user.save
-
+    user = recognize_user(provider) or create_user(provider)
     reset_session
     auto_login user, true
+  ensure
     redirect_to '/'
   end
 
   private
+
+  def recognize_user provider
+    user_hash = sorcery_fetch_user_hash(provider)
+    uid = user_hash[:uid]
+    authentication = Authentication.find_by_uid(uid)
+    authentication.try :user
+  end
+
+  def create_user provider
+    user = create_and_validate_from provider
+    user.generate_password
+    user.generate_email
+    user.save
+    user
+  end
 
   def auth_params
     params.permit :code, :provider
