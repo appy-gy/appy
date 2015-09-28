446f9d80f3ddedfee7a55df8bc2ed58c635796ba
Anton Kalinin <asgard.ssh@gmail.com>
2015-09-28 11:12:04 +0300

    fix oauths controller

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,7 +8,7 @@ class OauthsController < ApplicationController
   def callback
     provider = auth_params[:provider]
     return if login_from provider
-    user = recognize_user(provider) or create_user(provider)
+    user = recognize_user(provider) || create_user(provider)
     return unless user
     reset_session
     auto_login user, true
