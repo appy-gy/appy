c065a61b08e290f72a3a12f48e47dd20b4a40399
miraks <a@vldkn.net>
2015-11-22 18:54:44 +0300

    fix api incompability when serialize object empty image field

--- api/lib/top/image_uploader.ex
+++ api/lib/top/image_uploader.ex
@@ -1,6 +1,7 @@
 defmodule Top.ImageUploader do
   defmacro image(field) do
     quote do
+      def unquote(:"#{field}_url")(%{unquote(field) => nil}), do: nil
       def unquote(:"#{field}_url")(record) do
         id = String.replace record.id, "-", "/"
         "/system/#{model_name}/#{unquote(field)}/#{id}/#{record.unquote(field)}"
