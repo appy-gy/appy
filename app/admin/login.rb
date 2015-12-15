6f36b6287c948681b94c2119582812aecf453738
miraks <a@vldkn.net>
2015-12-15 22:22:04 +0300

    add login as to admin panel

---
+++ app/admin/login.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register_page 'Login' do
+  menu label: 'Войти как', priority: 6
+
+  page_action :as, method: :post
+
+  content do
+    render partial: 'form'
+  end
+
+  controller do
+    include Sorcery::Controller
+
+    def as
+      user = User.find_by email: params[:email]
+      return redirect_to :back unless user
+      auto_login user
+      redirect_to '/'
+    end
+  end
+end
