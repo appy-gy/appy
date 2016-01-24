82889270dcbd8390895ab6ee8437733d32573824
miraks <a@vldkn.net>
2016-01-24 15:38:57 +0300

    order notifications

--- app/controllers/api/private/browser_notifications_controller.rb
+++ app/controllers/api/private/browser_notifications_controller.rb
@@ -2,7 +2,7 @@ module Api
   module Private
     class BrowserNotificationsController < BaseController
       def show
-        notification = BrowserNotification.recent.of(current_user).last
+        notification = BrowserNotification.recent.of(current_user).order(:created_at).last
         render json: notification
       end
     end
