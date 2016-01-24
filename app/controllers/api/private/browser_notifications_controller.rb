2a6194b8f52669021ce32fa6b64b1ea7683eee2d
Anton <asgard.ssh@gmail.com>
2016-01-24 16:06:14 +0300

    fix browser notofocation serialization

--- app/controllers/api/private/browser_notifications_controller.rb
+++ app/controllers/api/private/browser_notifications_controller.rb
@@ -2,8 +2,8 @@ module Api
   module Private
     class BrowserNotificationsController < BaseController
       def show
-        notification = BrowserNotification.recent.of(current_user).last
-        render json: notification
+        notification = BrowserNotification.recent.of(User.find('api')).last
+        render json: notification, serializer: BrowserNotificationSerializer
       end
     end
   end
